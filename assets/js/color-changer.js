2b6de500edb37a3b5940643237938db0166f9e39
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-03-20 14:11:39 +0100

    Made sweet even sweeter

--- assets/js/color-changer.js
+++ assets/js/color-changer.js
@@ -1,6 +1,18 @@
-function colorChanger(color){
-    var color = color
-       return  document.getElementById("body").style.backgroundColor = color;
+$(function() {
+    var prev_class = '';
     
-}
+    $('.round').click(function(e) {
+        // disallow the boring hash in the url
+        e.preventDefault();
 
+        // figure out which class to apply - and apply it
+        var css_class = $(this).data('class');
+        $('body').addClass(css_class);
+
+        // remove previous class
+        if ( $('body').hasClass(prev_class) ) $('body').removeClass(prev_class);
+
+        // take note of this class
+        prev_class = css_class;
+    });
+});
